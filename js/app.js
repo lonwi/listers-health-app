b84d628f78afe03c649f02f14dcaa24d181b32b9
Wojciech Bubolka <wojciech@bubolka.com>
2016-02-17 09:13:38 +0000

    Analytics Test

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 (function () {
 	'use strict';
 	
-	angular.module('listershealth', ['ionic','ionic.service.core', 'listershealth.controllers', 'listershealth.services', 'listershealth.filters', 'ngCordova', 'angular-cache', 'angulartics', 'angulartics.google.analytics'])
+	angular.module('listershealth', ['ionic','ionic.service.core', 'listershealth.controllers', 'listershealth.services', 'listershealth.filters', 'ngCordova', 'angular-cache'])
 	
 	.run(function($ionicPlatform, $ionicPopup) {
 		
@@ -33,7 +33,22 @@
 					autoShow : true
 				});
 			}
+			
+			if(window.analytics){ 
+				window.analytics.startTrackerWithId('UA-73141751-2');
+				window.analytics.trackView('Home');
+			}
+			
+			/*
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			
+			  ga('create', 'UA-73141751-3', 'auto');
+			*/  
 		});
+			
 		
 		$ionicPlatform.ready(function() {
 			
